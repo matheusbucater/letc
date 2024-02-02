39c225e0f33fa38b3621772f7b143ba20804102f
Matheus Pelegrini Bucater <matheusbucater@ufu.br>
2024-02-02 12:01:02 -0300

    insert_at, update_at, delete_at, sort

--- circular-linked-list/main.c
+++ circular-linked-list/main.c
@@ -68,40 +68,166 @@ int length(struct node *head) {
     return l;
 }
 
-// void delete(struct node **head, int index) {
-//     
-//     struct node *prev_ptr = NULL;
-//     struct node *ptr = *head;
-//
-//     int 
-//
-//     if ( index == 0 ) {
-//         *head = (*head)->next;
-//     }
-//
-//     for ( int i = 0; i < index; i++ ) {
-//         prev_ptr = ptr;
-//         ptr = ptr->next;
-//     }
-//
-//     prev_ptr->next = ptr->next;
-//
-//     free(ptr);
-//
-// }
+void insert_at(struct node **head, int index, int data) {
+    
+    struct node *new_node = (struct node*)malloc(sizeof(struct node));
+    new_node->data = data;
+
+    if ( *head == NULL ) {
+        *head = new_node;
+        (*head)->next = *head;
+        return;
+    }
+
+    if ( (*head)->next == *head ) {
+        new_node->next = *head;
+        (*head)->next = new_node;
+        *head = new_node;
+        return;
+    }
+
+
+    struct node *prev_ptr = NULL;
+    struct node *ptr = *head;
+    
+    if ( index == 0 ) {
+
+        do {
+            ptr = ptr->next;
+        }
+        while ( ptr->next != *head );
+
+        ptr->next = new_node;
+        new_node->next = *head;
+        *head = new_node;
+        return;
+    }
+
+    for ( int i = 0; i < index; i++ ) {
+        prev_ptr = ptr;
+        ptr = ptr->next;
+    }
+
+    prev_ptr->next = new_node;
+    new_node->next = ptr;
+
+}
+
+void update_at(struct node **head, int index, int data) {
+
+    if ( *head == NULL ) {
+        printf("err. head is null!");
+        return;
+    }
+
+    if ( (*head)->next == *head ) {
+        (*head)->data = data;
+        return;
+    }
+
+    struct node *ptr = *head;
+    for ( int i = 0; i < index; i++ ) {
+        ptr = ptr->next; 
+    }
+
+    ptr->data = data;
+}
+
+void delete_at(struct node **head, int index) {
+    
+    if ( *head == NULL ) {
+        return;
+    }
+
+    if ( (*head)->next == *head ) {
+        *head = NULL;
+        return;
+    }
+
+    if ( index == 0 ) {
+        *head = (*head)->next;
+        return;
+    }
+
+    struct node *prev_ptr = NULL;
+    struct node *ptr = *head;
+
+    for ( int i = 0; i < index; i++ ) {
+        prev_ptr = ptr;
+        ptr = ptr->next;
+    }
+
+    prev_ptr->next = ptr->next;
+
+    free(ptr);
+
+}
+
+
+void sort(struct node **head) {
+    struct node *sorted = (struct node*)malloc(sizeof(struct node));
+
+    if ( *head == NULL ) {
+        printf("err. head is null!");
+        return;
+    }
+
+    if ( (*head)->next == *head ) {
+        return;
+    }
+
+    while (1) {
+        struct node *prev_ptr = NULL;
+        struct node *ptr = *head;
+        int done = 0;
+
+        do {
+
+            prev_ptr = ptr;
+            ptr = ptr->next;
+
+            int prev_data = prev_ptr->data;
+            int data = ptr->data;
+
+            if ( prev_data > data ) {
+                prev_ptr->data = data;
+                ptr->data = prev_data;
+                done++;
+            }
+        }
+        while ( ptr->next != *head );
+
+        if ( done == 0 ) {
+            break;
+        }
+    }
+
+
+}
 
 int main(void) {
 
     struct node *head = NULL;
 
-    for ( int i = 0; i < 10; i++ ) {
-        insert(&head, i, 1);
-    }
+    // for ( int i = 0; i < 10; i++ ) {
+    //     insert(&head, i, 1);
+    // }
+    //
+
+    insert(&head, 1, 1);
+    insert(&head, 3, 1);
+    insert(&head, 2, 1);
+    insert(&head, 2, 1);
+    insert(&head, 5, 1);
+    insert(&head, 0, 1);
 
     display(head);
 
-    printf("%d\n", length(head));
+    sort(&head);
 
+    display(head);
+
+    
     free(head);
     return 0;
 }
